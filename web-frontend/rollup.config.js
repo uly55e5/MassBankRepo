36f9f71309793f1370010a3a4300c32cb905e608
David Rauh <drauh@ipb-halle.de>
2022-03-07 10:30:44 +0100

    add local node_modules dir

--- web-frontend/rollup.config.js
+++ web-frontend/rollup.config.js
@@ -54,7 +54,10 @@ export default {
 		// https://github.com/rollup/plugins/tree/master/packages/commonjs
 		resolve({
 			browser: true,
-			dedupe: ['svelte']
+			dedupe: ['svelte'],
+			customResolveOptions: {
+				moduleDirectory: 'node_modules'
+			}
 		}),
 		commonjs(),
 
