eadf3e540f644d7fa78015611090c8d28a6d24f8
David Rauh <drauh@ipb-halle.de>
2022-07-21 11:52:50 +0200

    Updated minio service

--- helper/massbankhelper.go
+++ helper/massbankhelper.go
@@ -1,23 +1,13 @@
 package main
 
-import (
-	"flag"
-	"github.com/uly55e5/MassBankRepo/helper/lib"
-	"path/filepath"
-)
+// #include "../../pwizmzlib/mz_pwiz_c_interface.h"
+// #cgo LDFLAGS: -L../../pwizmzlib/dist/debug -lpwizmzlib -lzstd -lbz2 -llzma
+// #cgo CFLAGS: -I../../pwizmzlib/libs/pwiz
+import "C"
 
 func main() {
-	//defer profile.Start(profile.ProfilePath(".")).Stop()
-	mbPath := flag.String("f", "", "path to massbank data")
-	outFile := flag.String("o", "mbParser-out.txt", "name of the output file")
-	stats := flag.Bool("s", false, "output statistics")
-	flag.Parse()
-	if *stats {
-		lib.SetMode(lib.Stats)
-	}
-	err := filepath.Walk(*mbPath, lib.ReadTags)
-	if err != nil {
-		println(err.Error())
-	}
-	lib.WriteList(*outFile)
+	var msdata *C.struct_MSDataFile = C.openMSData(C.CString("../../test-data/mzML_Files/005_TOM_CYS.mzML"))
+	idata := C.getInstrumentInfo(msdata)
+	runinfo := C.getRunInfo(msdata)
+	println(msdata, runinfo, idata)
 }
