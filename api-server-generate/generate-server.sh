2a741d7afa1c288c0082284bcef0cb5d6cce5e08
David Rauh <drauh@ipb-halle.de>
2022-07-21 12:55:36 +0200

    improved server generation

--- api-server-generate/generate-server.sh
+++ api-server-generate/generate-server.sh
@@ -1,2 +1,7 @@
-#!bin/sh
-node_modules/.bin/openapi-generator-cli generate -c api-server-generate/config.yaml
+#!/bin/sh
+WORK_DIR=$(pwd)
+SCRIPT_DIR=$(dirname $0)
+cd ${SCRIPT_DIR}
+pwd
+../node_modules/.bin/openapi-generator-cli generate -c config.yaml
+cd ${WORK_DIR}
